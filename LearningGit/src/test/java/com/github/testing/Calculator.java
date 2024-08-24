7fb997e60e13e8164ccbac625ab723e973cefe34
KUDYSAMA <kudysama@gmail.com>
2024-08-24 12:26:58 -0400

    A simple calculator program that performs addition, subtraction, multiplication, and division.

--- LearningGit/src/test/java/com/github/testing/Calculator.java
+++ LearningGit/src/test/java/com/github/testing/Calculator.java
@@ -1,10 +1,39 @@
+
 package com.github.testing;
 
+// A simple calculator program that performs addition, subtraction, multiplication, and division.
 public class Calculator {
+	
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
 
-	public static void main(String[] args) {
-		
-		System.out.println("Hello World!");
-	}
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
 
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static double divide(int num1, int num2) {
+        if (num2 != 0) {
+            return (double) num1 / num2;
+        } else {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is a simple calculator program.");
+        System.out.println("This program will perform the following operations:");
+
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println("Addition: " + add(num1, num2));
+        System.out.println("Subtraction: " + subtract(num1, num2));
+        System.out.println("Multiplication: " + multiply(num1, num2));
+        System.out.println("Division: " + divide(num1, num2));
+    }
 }
